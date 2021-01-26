49212ebb8b06b6d5b92999cb1c987bf15ffcd8ce
MustafaColakoglu <mustafacolakoglu7@gmail.com>
2021-01-26 20:50:08 +0300

    finilaze app

--- src/App.js
+++ src/App.js
@@ -1,79 +1,79 @@
-import React,{Component} from "react"
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 import MovieRow from "./MovieRow";
-import $ from "jquery"
+
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={}
+    this.state = {};
     this.performSearch("woman");
-
-    
   }
 
-  performSearch=(seacrhTerm)=>{
+  performSearch = (seacrhTerm) => {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${seacrhTerm}&api_key=f1e3d8a64dd65f44826c0c41033bebfd`;
 
-    const url=`https://api.themoviedb.org/3/search/movie?query=${seacrhTerm}&api_key=f1e3d8a64dd65f44826c0c41033bebfd`;
-      $.ajax({
-        url:url,
-        success:(searchResults)=>{
-          console.log(searchResults)
+    fetch(url)
+      .then((data) => data.json())
+      .then((result) => this.renderMovies(result))
+      .catch((err) => console.error(err));
+  };
 
-          let movieRows=[];
-          const results=searchResults.results;
+  renderMovies = (data) => {
+    let movieRows = [];
+    const results = data.results;
 
-          results.forEach(movie=>{
-            const movieRow=<MovieRow key={movie.id} movie={movie}></MovieRow>
-            movieRows.push(movieRow)
-          })
+    results.forEach((movie) => {
+      const movieRow = <MovieRow key={movie.id} movie={movie}></MovieRow>;
+      movieRows.push(movieRow);
+    });
 
-          this.setState({rows:movieRows})
-        },
-        error:(xhr,status,error)=>{
-          console.error("error")
-        }
-      })
-  }
+    this.setState({ rows: movieRows });
+  };
 
-  searchChangeHandler=(e)=>{
-    const {value}=e.target;
+  searchChangeHandler = (e) => {
+    const { value } = e.target;
 
     this.performSearch(value);
+  };
+
+  render() {
+    return (
+      <div>
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img
+                  width="100px"
+                  src="https://m.media-amazon.com/images/G/01/IMDb/BG_rectangle_white._CB1509060989_SY230_SX307_AL_.png"
+                  alt="imdb"
+                />
+              </td>
+              <td width="8" />
+              <td>
+                <h3>MoviesDB Search</h3>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input
+          style={{
+            fontSize: 24,
+            display: "block",
+            width: "99%",
+            paddingTop: 8,
+            paddingBottom: 8,
+            paddingLeft: 16,
+          }}
+          placeholder="Enter search term"
+          onChange={this.searchChangeHandler}
+        ></input>
+
+        {this.state.rows}
+      </div>
+    );
   }
-
-render(){
-  return (
-    <div>
-      <table className="titleBar" >
-        <tbody>
-          <tr>
-            <td>
-              <img width="50px" src="themoviedb.png" alt="imdb" />
-            </td>
-            <td width="8"/>
-            <td>
-              <h3>MoviesDB Search</h3>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-      <input style={{
-        fontSize:24,
-        display:"block",
-        width:"99%",
-        paddingTop:8,
-        paddingBottom:8,
-        paddingLeft:16
-        
-      }} placeholder="Enter search term" onChange={this.searchChangeHandler}></input>
-
-      {this.state.rows}
-    </div>
-  );
-}
- 
 }
 
 export default App;
